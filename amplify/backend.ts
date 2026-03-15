c2061433ba985561f0e827812decc8631996252c
Coder <vishnu.sharma7193240@gmail.com>
2026-03-15 00:14:52 -0400

    attempt at adding data resource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,10 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage } from './storage/resource';
 import { sayHello } from './functions/say-hello/resource';
-
+import { data } from './data/resource';
 defineBackend({ 
   auth,
+  data,
   storage,
   sayHello
 });
