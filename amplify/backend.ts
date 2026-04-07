cf03e80f014629db6ccc440c700b75ffba76d9f6
Coder <vishnu.sharma7193240@gmail.com>
2026-04-07 16:24:05 -0400

    remove necessity for graphql apis

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,14 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage } from './storage/resource';
-import { data } from './data/resource';
 
 export const backend = defineBackend({ 
   auth,
-  data,
   storage
 });
-
 
 
 
